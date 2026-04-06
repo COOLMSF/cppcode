0b033cb06c07354599d0ad5a6f680ec96d62ca3e
coolder <coolder.hsl@outlook.com>
2026-04-06 11:28:06 +0800

    Remove build directory from repository

--- build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,3 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
-  /Users/coolder/CascadeProjects/cc-haha-cpp/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
-  /Users/coolder/CascadeProjects/cc-haha-cpp/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
